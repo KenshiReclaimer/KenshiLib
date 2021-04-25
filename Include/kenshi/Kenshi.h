620964632459f968ccf17187d719eac9de97ea7c
Bryden Frizzell <bryden.frizzell@gmail.com>
2021-04-25 17:02:07 +1200

    Added hash-based version detection  + GOG 1.0.51 GameWorld offset

--- Include/kenshi/Kenshi.h
+++ Include/kenshi/Kenshi.h
@@ -24,6 +24,58 @@ namespace Kenshi
 
     MyGUI::WidgetPtr FindWidget(MyGUI::EnumeratorWidgetPtr enumerator, std::string name);
 
+    class BinaryVersion
+    {
+    public:
+        enum KenshiPlatform
+        {
+            GOG,
+            STEAM,
+            UNKNOWN
+        };
+
+        BinaryVersion(KenshiPlatform platform, std::string version)
+            : platform(platform), version(version)
+        {
+
+        }
+
+        KenshiPlatform GetPlatform()
+        {
+            return platform;
+        }
+
+        std::string GetPlatformStr()
+        {
+            if (platform == KenshiPlatform::STEAM)
+                return "Steam";
+            if (platform == KenshiPlatform::GOG)
+                return "GOG";
+            return "Unknown";
+        }
+
+        std::string GetVersion()
+        {
+            return version;
+        }
+
+        // used for sorting/map
+        // TODO should use unordered map?
+        bool operator< (const BinaryVersion& b) const
+        {
+            if (this->version != b.version)
+                return this->version < b.version;
+            return this->platform < b.platform;
+        }
+
+    private:
+        KenshiPlatform platform;
+        // TODO should this be ints?
+        std::string version;
+    };
+
+    BinaryVersion GetKenshiVersion();
+
     GameWorld& GetGameWorld();
     // GameWorld looks to be an object of all game state, whether its loaded or not.
     // Has things like a list of loaded mods and all the GameData loaded in, probably saves too.
--- Source/kenshi/Kenshi.cpp
+++ Source/kenshi/Kenshi.cpp
@@ -6,6 +6,8 @@
 #include <core/RVA.h>
 #include <cassert>
 
+#include <core/md5.h>
+
 /**
  * Kenshi.cpp
  * 
@@ -15,10 +17,67 @@
  * 
  */
 
+std::string GetEXEHash()
+{
+    WCHAR path[MAX_PATH];
+    GetModuleFileNameW(NULL, path, MAX_PATH);
+    std::ifstream file(std::wstring(path), std::ios::ate | std::ios::binary);
+    if (file.is_open())
+    {
+        size_t size = file.tellg();
+        std::vector<char> fileBytes(size);
+        file.seekg(0);
+        file.read(&fileBytes[0], fileBytes.size());
+        md5::md5_t hasher(&fileBytes[0], fileBytes.size());
+        char hashStr[33];
+        hasher.get_string(hashStr);
+        return std::string(hashStr);
+    }
+    return "ERROR";
+}
+
+// nicer static initialization in C++03
+template<typename T1, typename T2>
+class StaticMap : public std::map<T1, T2>
+{
+public:
+    StaticMap()
+        : std::map<T1, T2>()
+    {
+
+    }
+    StaticMap& Add(T1 key, T2 value)
+    {
+        this->insert(std::make_pair(key, value));
+        return *this;
+    }
+};
+
+StaticMap<std::string, Kenshi::BinaryVersion> HashToVersionMap = StaticMap<std::string, Kenshi::BinaryVersion>()
+    .Add("a5f78908f3f26591a6a3717bfbfafbca", Kenshi::BinaryVersion(Kenshi::BinaryVersion::STEAM, "1.0.51")) // RE_Kenshi 0.1 modified EXE
+    .Add("57679de0ae258ead45a96949974517e2", Kenshi::BinaryVersion(Kenshi::BinaryVersion::STEAM, "1.0.51")) // Unmodified
+    .Add("525261fca4d339da67999c114118c6c6", Kenshi::BinaryVersion(Kenshi::BinaryVersion::GOG, "1.0.51"));
+
+// Game world address Steam 1.51.1 0x001AAE060
+// Game world address GOG 1.51.1 0x001AADFB0
+StaticMap<Kenshi::BinaryVersion, offset_t> GameWorldOffset = StaticMap<Kenshi::BinaryVersion, offset_t>()
+    .Add(Kenshi::BinaryVersion(Kenshi::BinaryVersion::STEAM, "1.0.51"), 0x001AAE060)
+    .Add(Kenshi::BinaryVersion(Kenshi::BinaryVersion::GOG, "1.0.51"), 0x001AADFB0);
+
+
+std::string kenshiHash = GetEXEHash();
+Kenshi::BinaryVersion kenshiVersion = HashToVersionMap.count(kenshiHash) > 0 ? HashToVersionMap.at(kenshiHash) : Kenshi::BinaryVersion(Kenshi::BinaryVersion::UNKNOWN, "UNKNOWN");
+
+Kenshi::BinaryVersion Kenshi::GetKenshiVersion()
+{
+    return kenshiVersion;
+}
 
 Kenshi::GameWorld& Kenshi::GetGameWorld()
 {
-    static RVAPtr<GameWorld> c_inst(0x001AAE060);
+    Kenshi::BinaryVersion kenshiVersion = GetKenshiVersion();
+    offset_t gameWorldOffset = GameWorldOffset.at(kenshiVersion);
+    static RVAPtr<GameWorld> c_inst(gameWorldOffset);
     return *c_inst.GetPtr();
 }
 
