74d264f6781ec880e21659a9883888a5e8ea646f
Greg <greglando@gmail.com>
2021-08-21 11:26:43 -0700

    added tasker abc

--- Include/kenshi/Tasks.h
+++ Include/kenshi/Tasks.h
@@ -3,9 +3,15 @@
 namespace Kenshi 
 {
 
-    struct Tasker 
+    struct Tasker // TBD, still working
     {
+        virtual ~Tasker();
 
+        virtual void Start(Tasker* task, /* CharBody* usually */ void* owner);
+        virtual void Update(Tasker* task, /* CharBody* usually */ void* owner);
+        virtual void Complete(Tasker* task, /* CharBody* usually */ void* owner);
+
+        virtual bool IsComplete(Tasker* task);
 
     };
 
