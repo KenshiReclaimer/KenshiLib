a0e77159cb76d7678740e7c44e01ef65bc43371f
Bryden Frizzell <bryden.frizzell@gmail.com>
2022-01-03 15:32:29 +1300

    Added reversed structures for items + inventory, fixed some string length/alignment issues

--- Include/kenshi/Character.h
+++ Include/kenshi/Character.h
@@ -11,6 +11,7 @@ namespace Kenshi
 
     struct GameData;
     struct Faction;
+    struct Inventory;
 
     struct Character : public RootObject
     {
@@ -20,15 +21,15 @@ namespace Kenshi
     	uint64_t id; //0x0008
         Faction *faction; //0x0010
         std::string name; //0x0018
-        char pad_0038[8]; //0x0038
         GameData *dataNode; //0x0040
         Ogre::Vector3 position; //0x0048
         char pad_0054[92]; //0x0054
         Ogre::Vector3 rotation; //0x00B0
         char pad_00BC[8]; //0x00BC
         Ogre::Vector3 N00000E8C; //0x00C4
-        char pad_00D0[904]; //0x00D0
-        // inventory // 0x2E8
+        char pad_00D0[0x218]; //0x00D0
+        Inventory* inventory; // 0x02E8
+        char pad_02F0[0x168]; // 0x02F0
         MedicalSystem medicalSystem; //0x0458 MedicalSystem
     };
 
