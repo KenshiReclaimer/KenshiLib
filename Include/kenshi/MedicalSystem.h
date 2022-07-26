b5a6cc72e7bdfaf46cba309c454da4275932747c
Bryden Frizzell <bryden.frizzell@gmail.com>
2022-07-26 13:59:26 +1200

    Reversed more health/damage related structs

--- Include/kenshi/MedicalSystem.h
+++ Include/kenshi/MedicalSystem.h
@@ -13,6 +13,18 @@ namespace Kenshi
     struct AnimationClass;
     struct Character;
 
+
+    // Not 100% sure this is correct
+    struct Damage
+    {
+        float cutDamage; // 0x00
+        float bluntDamage; // 0x04
+        float unk1; // 0x08
+        float finalBlunt; //0x0C not really sure about what this does
+        float bleedMult;
+        float armourPenetration;
+    };
+
     // possibly 0x1B0 long? 
     // ^ based on highest byte written by constructor + next byte written to in Character constructor
 
@@ -36,34 +48,38 @@ namespace Kenshi
             A limb, blood, oil, hunger arent here, just head/arms/legs/other appendages.
         */
         {
-            GameData* data;
-            uint64_t type; // ? unsure, different for different limbs, but not unique
-            MedicalSystem* medical;
-            Character* character;
-            uint64_t unknown[3]; // theres some boolean flags in the last int in this group
-            float unknown1; // currently 80, some hidden stat for limb health?
-            float unknown2; // currently 1, some regeneration stat?
-            float health; // the current health of the limb.
-            float unknown3;
+            GameData* data; // 0x00
+            uint64_t type; // 0x08 // ? unsure, different for different limbs, but not unique
+            MedicalSystem* medical; // 0x10
+            Character* character; // 0x18
+            uint8_t unknown[0x18]; // 0x20 maybe theres some boolean flags in the last int in this group
+            float unknown1; // 0x38 currently 80, some hidden stat for limb health?
+            float unknown2; // 0x3C currently 1, some regeneration stat?
+            float health; // 0x40 the current health of the limb.
+            float bluntDamage; // 0x44
+            char unk_0048[0xC]; // 0x48
+            float maxHealth; // 0x54
+            float unknown4; // 0x58 -> 1.0
+            float unknown5; // 0x5C -> 1.0
+            float unknown6; // 0x60 -> 1.0
         };
 
+        // boost::unordered hashmap around beginning
         char pad_0008[64]; //0x0008
-        class lektor<void*> N00000F07; //0x0048
+        // this is looped through to calculate armour damage reduction when damaged
+        class lektor<Armour*> equippedArmour; //0x0048
         float hunger; //0x0060
         char pad_0064[12]; //0x0064
         float blood; //0x0070
-        char pad_0074[116]; //0x0074
+        char pad_0074[100]; //0x0074
+        AnimationClass* animationClass; // 0xD8
+        Character* character; // 0xE0
         class StringPair environmentStatus; //0x00E8 The state above the healthbars
         char pad_0140[80]; //0x0140
         class lektor<HealthPartStatus> healthParts; //0x0190 HealthPartStatus
-        char pad_01A8[16]; //0x01A8
-        std::string gender; //0x01B8
-        char pad_01E0[56]; //0x01E0
-        Ogre::Vector3 position; //0x0218
-        Ogre::Vector3 rotation; //0x0224
-        char pad_0230[48]; //0x0230
-        class lektor<Effect*> healthEffects; //0x0260 Effect*
-        char pad_0278[96]; //0x0278
+        CharStats* charStats;
+        // is this actually a part of MedicalSystem? the constructor/destructor only deals with up to 0x190
+        
         
         virtual void Function0();
         virtual void Function1();
