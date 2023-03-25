271c9b5962ccd12c86fd0afad38c52b09135eb95
Bryden Frizzell <bryden.frizzell@gmail.com>
2023-03-26 10:31:48 +1300

    Added timestamps to errors

--- Source/Debug.cpp
+++ Source/Debug.cpp
@@ -27,8 +27,17 @@ void DebugLog(std::string message)
 
 void ErrorLog(std::string message)
 {
-	debugFile << "Error: " << message << std::endl;
-	debugLog << "Error: " << message << std::endl;
+	if (startTime == -1)
+		startTime = clock();
+
+	clock_t logTime = clock();
+	clock_t timeInMSecs = (logTime - startTime) / (CLOCKS_PER_SEC / 1000);
+
+	char timeStr[10];
+	sprintf_s(timeStr, "%i.%03i", timeInMSecs / 1000, timeInMSecs % 1000);
+
+	debugFile << " Error " << timeStr << ": " << message << std::endl;
+	debugLog << " Error " << timeStr << ": " << message << std::endl;
 }
 
 std::string GetDebugLog()
